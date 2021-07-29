8c8f0fc211272818b3685edd30a4a2bf465e0e4c
AnuragAnalog <anurag.peddi1998@gmail.com>
2021-07-29 19:30:03 +0530

    added download sh file

--- downloads.sh
+++ downloads.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Get packages required for OpenCV
-
-sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
-sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get -y install libxvidcore-dev libx264-dev
-sudo apt-get -y install qt4-dev-tools libatlas-base-dev
+sudo apt install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt install libxvidcore-dev libx264-dev -y
+sudo apt install qt4-dev-tools libatlas-base-dev -y
+sudo apt install libilmbase-dev libopenexr-dev libgstreamer1.0-dev -y
 
 # Need to get an older version of OpenCV because version 4 has errors
-pip3 install opencv-python==3.4.6.27
+pip3 install opencv-python
 
 # Get packages required for TensorFlow
-# Using the tflite_runtime packages available at https://www.tensorflow.org/lite/guide/python
-# Will change to just 'pip3 install tensorflow' once newer versions of TF are added to piwheels
+# pip3 install tensorflow (uncomment to install tensorflow)
 
-#pip3 install tensorflow
+# To install tflite runtime
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install python3-tflite-runtime
